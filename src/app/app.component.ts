50313af5d2d2d38e24c3a7e2ffe181a10242c53e
Eluzive <kamil.p@gmx.com>
2018-10-13 12:48:20 +0200

    Interpolation use case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ 'HELLO' | translate:{firstName: 'Eluzive'} }}</h1>
+  `,
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
 }
